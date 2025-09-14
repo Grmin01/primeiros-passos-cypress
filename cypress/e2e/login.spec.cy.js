03463b482a8e40c28756892db11c184ba45679fe
Alex Sandro de Paula silva <grmin21@gmail.com>
2025-09-14 16:44:11 -0300

    Adding user data json

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,3 +1,5 @@
+import userData from '../fixtures/userData.json'
+
 describe('orange HRM Tests', () => {
   //criando um objeto
   const selectorsList = {
@@ -5,25 +7,39 @@ describe('orange HRM Tests', () => {
     passwordField: "[name='password']",
     loguinButton: "[type='submit']",
     selectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    dashboardGrid: ".orangehrm-dashboard-grid",
     wrongCredencialAlert: "[role='alert']"
   } 
+   
+  //Usando dados de testes em objetos
+  //const userDate = {
+    //userSuccess: {
+      //username: 'Admin',
+      //password: 'admin123'
+    //},
+    //userFail: {
+     // username: 'Teste',
+      //password: 'Teste'
+   // }
+  //}
 
   it('Login - Success', () => {  // quando quiser pular uma aprte do codigo usar .skip
     cy.visit('https:/opensource-demo.orangehrmlive.com/web/index.php/auth/login') // abriando a pagina
     //Loguin usuario e senha
-    cy.get(selectorsList.usernameField).type('Admin') //Usuario
-    cy.get(selectorsList.passwordField).type('admin123')  // Senha
+    cy.get(selectorsList.usernameField).type(userData.userSuccess.username) //Usuario
+    cy.get(selectorsList.passwordField).type(userData.userSuccess.password)  // Senha
     cy.get(selectorsList.loguinButton).click() //Clicar no botao de loguin
 
     //Verificar se esta na pagina correta
     //cy.location('pathname').should('equal', '/web/index.php/dashboard/index') //pelo site
-    cy.get(selectorsList.selectionTitleTopBar).contains('Dashboar') //confirma por palavras cotinda em algum titulo
+    //cy.get(selectorsList.selectionTitleTopBar).contains('Dashboar') //confirma por palavras cotinda em algum titulo
+    cy.get(selectorsList.dashboardGrid)  // verificando so com elemento existente
   })
   it('Login - Fail', () => {
     cy.visit('https:/opensource-demo.orangehrmlive.com/web/index.php/auth/login') // abriando a pagina
     //Loguin usuario e senha
-    cy.get(selectorsList.usernameField).type('Test') //Usuario
-    cy.get(selectorsList.passwordField).type('Test')  // Senha
+    cy.get(selectorsList.usernameField).type(userData.userFail.username) //Usuario
+    cy.get(selectorsList.passwordField).type(userData.userFail.password)  // Senha
     cy.get(selectorsList.loguinButton).click() //Clicar no botao de loguin
     cy.get(selectorsList.wrongCredencialAlert) //Alerta de erro
   })
