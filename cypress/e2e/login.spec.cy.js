171d76b552694aeb5ed84f7f81306540d01f0134
Alex Sandro de Paula silva <grmin21@gmail.com>
2025-09-14 14:52:41 -0300

    Added selectors list for login spec

--- cypress/e2e/login.spec.cy.js
+++ cypress/e2e/login.spec.cy.js
@@ -1,21 +1,30 @@
 describe('orange HRM Tests', () => {
+  //criando um objeto
+  const selectorsList = {
+    usernameField: "[name='username']",
+    passwordField: "[name='password']",
+    loguinButton: "[type='submit']",
+    selectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
+    wrongCredencialAlert: "[role='alert']"
+  } 
+
   it('Login - Success', () => {  // quando quiser pular uma aprte do codigo usar .skip
     cy.visit('https:/opensource-demo.orangehrmlive.com/web/index.php/auth/login') // abriando a pagina
     //Loguin usuario e senha
-    cy.get("[name='username']").type('Admin') //Usuario
-    cy.get("[name='password']").type('admin123')  // Senha
-    cy.get("[type='submit']").click() //Clicar no botao de loguin
+    cy.get(selectorsList.usernameField).type('Admin') //Usuario
+    cy.get(selectorsList.passwordField).type('admin123')  // Senha
+    cy.get(selectorsList.loguinButton).click() //Clicar no botao de loguin
 
     //Verificar se esta na pagina correta
     //cy.location('pathname').should('equal', '/web/index.php/dashboard/index') //pelo site
-    cy.get(".oxd-topbar-header-breadcrumb-module").contains('Dashboar') //confirma por palavras cotinda em algum titulo
+    cy.get(selectorsList.selectionTitleTopBar).contains('Dashboar') //confirma por palavras cotinda em algum titulo
   })
   it('Login - Fail', () => {
     cy.visit('https:/opensource-demo.orangehrmlive.com/web/index.php/auth/login') // abriando a pagina
     //Loguin usuario e senha
-    cy.get("[name='username']").type('Test') //Usuario
-    cy.get("[name='password']").type('Test')  // Senha
-    cy.get("[type='submit']").click() //Clicar no botao de loguin
-    cy.get("[role='alert']") //Alerta de erro
+    cy.get(selectorsList.usernameField).type('Test') //Usuario
+    cy.get(selectorsList.passwordField).type('Test')  // Senha
+    cy.get(selectorsList.loguinButton).click() //Clicar no botao de loguin
+    cy.get(selectorsList.wrongCredencialAlert) //Alerta de erro
   })
 })
