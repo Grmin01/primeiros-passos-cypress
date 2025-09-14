0ec8905db313f6c2ee357c48c879e6e14232f421
Alex Sandro de Paula silva <grmin21@gmail.com>
2025-09-14 19:50:45 -0300

    user update scenario added

--- cypress/e2e/user.spec.cy.js
+++ cypress/e2e/user.spec.cy.js
@@ -8,7 +8,18 @@ describe('orange HRM Tests', () => {
     loguinButton: "[type='submit']",
     selectionTitleTopBar: ".oxd-topbar-header-breadcrumb-module",
     dashboardGrid: ".orangehrm-dashboard-grid",
-    wrongCredencialAlert: "[role='alert']"
+    wrongCredencialAlert: "[role='alert']",
+    myInfoButtom: "[href='/web/index.php/pim/viewMyDetails']",
+    firstNameField: "[name='firstName']",
+    middleNameField: "[name='middleName']",
+    lastNameField: "[name='lastName']",
+    employeeIdField: ".oxd-input",
+    otherIdField: ".oxd-input",
+    driversLicenseNumberField: ".oxd-input",
+    licenseExpiryDateField: "[placeholder='yyyy-dd-mm']",
+    dateCloseButton: ".--close",
+    submitButton: "[type='submit']",
+    
   } 
    
   //Usando dados de testes em objetos
@@ -23,18 +34,32 @@ describe('orange HRM Tests', () => {
    // }
   //}
 
-  it('Login - Success', () => {  // quando quiser pular uma aprte do codigo usar .skip
+  it.only('User Infor Update - Success', () => {  // quando quiser pular uma aprte do codigo usar .skip
     cy.visit('/auth/login') // abriando a pagina
     //Loguin usuario e senha
     cy.get(selectorsList.usernameField).type(userData.userSuccess.username) //Usuario
     cy.get(selectorsList.passwordField).type(userData.userSuccess.password)  // Senha
     cy.get(selectorsList.loguinButton).click() //Clicar no botao de loguin
-
+    
     //Verificar se esta na pagina correta
     //cy.location('pathname').should('equal', '/web/index.php/dashboard/index') //pelo site
     //cy.get(selectorsList.selectionTitleTopBar).contains('Dashboar') //confirma por palavras cotinda em algum titulo
     cy.get(selectorsList.dashboardGrid)  // verificando so com elemento existente
+    cy.get(selectorsList.myInfoButtom).click()
+    cy.get(selectorsList.firstNameField).clear().type(userData.myInfoField.firstName)
+    cy.get(selectorsList.middleNameField).clear().type(userData.myInfoField.middleName)
+    cy.get(selectorsList.lastNameField).clear().type(userData.myInfoField.lastname)  
+    cy.get(selectorsList.employeeIdField).eq(4).clear().type(userData.myInfoField.employeeId) 
+    cy.get(selectorsList.otherIdField).eq(5).clear().type(userData.myInfoField.otherId)
+    cy.get(selectorsList.driversLicenseNumberField).eq(6).clear().type(userData.myInfoField.driversLicenseNumber)    
+    cy.get(selectorsList.licenseExpiryDateField).eq(0).clear().type(userData.myInfoField.licenseExpiryDate)
+    cy.get(selectorsList.dateCloseButton).click()
+    cy.get(selectorsList.submitButton).eq(0).click()
+    cy.get('body').should('contain', 'Successfully Updated')
+
   })
+
+
   it('Login - Fail', () => {
     cy.visit('/auth/login') // abriando a pagina
     //Loguin usuario e senha
